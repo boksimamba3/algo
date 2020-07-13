280cd92a78f1246860c4f14f16dc1d375325989c
boksimamba3 <boriskostres0@gmail.com>
2020-07-13 19:46:17 +0200

    Reverse list

--- arrays/main.go
+++ arrays/main.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-// List of 5 elements
+// List is slice of ints
 type List []int
 
 // Search finds elements in list
@@ -20,6 +20,26 @@ func (list List) Search(n int) int {
 	return -1
 }
 
+// IsSorted checks if list is sorted
+func (list List) IsSorted() bool {
+	for i := 0; i < len(list)-1; i++ {
+		if list[i] > list[i+1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Reverse list
+func (list List) Reverse() {
+	n := len(list)
+	for i := 0; i < n/2; i++ {
+		list[i], list[n-1-i] = list[n-1-i], list[i]
+	}
+}
+
+// String formats output
 func (list List) String() string {
 	var b bytes.Buffer
 	l := len(list)
@@ -33,21 +53,12 @@ func (list List) String() string {
 	return b.String()
 }
 
-/* func search(arr [5]int, n int) int {
-	for i := 0; i < len(arr); i++ {
-		if n == arr[i] {
-			return i
-		}
-	}
-
-	return -1
-}
-*/
 func main() {
-	// var arr [5]int = [5]int{1, 2, 3, 4, 5}
-	/* arr := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(search(arr, 3)) */
 	list := List{1, 2, 3, 4, 5}
-	fmt.Println(list.Search(4))
+	//fmt.Println(list.Search(4))
+	//fmt.Println(list.IsSorted())
+	list.Reverse()
+	fmt.Println(list)
+	list.Reverse()
 	fmt.Println(list)
 }
