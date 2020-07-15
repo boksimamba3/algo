90350048b709e0f723f5c00aaffba6417d0b53bb
boksimamba3 <boriskostres0@gmail.com>
2020-07-15 14:22:17 +0200

    Improved left rotate

--- arrays/left-rotate/main.go
+++ arrays/left-rotate/main.go
@@ -3,26 +3,17 @@ package main
 import "fmt"
 
 // Left rotate by one place
-/* func leftRotate(arr []int) []int {
-	if len(arr) == 0 {
-		return arr
-	}
-
+/* func leftRotate(arr []int) {
 	temp := arr[0]
 	for i := 1; i < len(arr); i++ {
 		arr[i-1] = arr[i]
 	}
 	arr[len(arr)-1] = temp
-
-	return arr
 } */
 
-// Left rotate by n places
-func leftRotate(arr []int, d int) []int {
+// Left rotate by n places Time O(N) Space O(d)
+/* func leftRotate(arr []int, d int) {
 	n := len(arr)
-	if n == 0 {
-		return arr
-	}
 
 	tempArr := []int{}
 	for i := 0; i < d; i++ {
@@ -34,11 +25,26 @@ func leftRotate(arr []int, d int) []int {
 	for i := 0; i < len(tempArr); i++ {
 		arr[n-d+i] = tempArr[i]
 	}
+} */
 
-	return arr
+// Left rotate by n places Time O(N) Space O(1)
+func leftRotate(arr []int, d int) {
+	n := len(arr)
+	reverse(arr, 0, d-1)
+	reverse(arr, d, n-1)
+	reverse(arr, 0, n-1)
+}
+
+func reverse(arr []int, low int, high int) {
+	for low < high {
+		arr[low], arr[high] = arr[high], arr[low]
+		low++
+		high--
+	}
 }
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5}
-	fmt.Println(leftRotate(arr, 4))
+	leftRotate(arr, 2)
+	fmt.Println(arr)
 }
