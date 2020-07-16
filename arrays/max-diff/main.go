73f88e01f10d2f71bc15f1a452984f9142791727
boksimamba3 <boriskostres0@gmail.com>
2020-07-16 17:02:59 +0200

    Max difference optimal solution

--- arrays/max-diff/main.go
+++ arrays/max-diff/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func maxDiff(arr []int) int {
+// Maximun value of arr[j] - arr[i] such that j > i
+/* func maxDiff(arr []int) int {
 	maxDiff := 0
 
 	for j := 1; j < len(arr); j++ {
@@ -13,6 +16,32 @@ func maxDiff(arr []int) int {
 		}
 	}
 
+	return maxDiff
+} */
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func maxDiff(arr []int) int {
+	maxDiff := arr[1] - arr[0]
+	minVal := arr[0]
+	for j := 1; j < len(arr); j++ {
+		maxDiff = max(maxDiff, arr[j]-minVal)
+		minVal = min(minVal, arr[j])
+	}
 	return maxDiff
 }
 
