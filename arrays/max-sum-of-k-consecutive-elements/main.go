992cc41a6cb53637914b0daec7b4b446639b156c
boksimamba3 <boriskostres0@gmail.com>
2020-09-28 17:22:16 +0200

    Max sum of k consecutive elements

---
+++ arrays/max-sum-of-k-consecutive-elements/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func maxSum(arr []int, k int) int {
+	if k > len(arr) {
+		return 0
+	}
+
+	sum := 0
+
+	for i := 0; i < k; i++ {
+		sum += arr[i]
+	}
+
+	currSum := sum
+
+	for start, end := 0, k; end < len(arr); start, end = start+1, end+1 {
+		currSum += arr[end] - arr[start]
+		sum = max(sum, currSum)
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(maxSum([]int{1, 8, 30, -5, 20}, 3))
+}
