7e6303154345877be05f92a950afe801e0d84e2c
boksimamba3 <boriskostres0@gmail.com>
2020-10-06 21:24:59 +0200

    Keep track of the smallest element index

--- sorting/selection-sort/main.go
+++ sorting/selection-sort/main.go
@@ -5,16 +5,18 @@ import "fmt"
 // O(N^2)
 func sort(arr []int) {
 	for i := 0; i < len(arr); i++ {
+		min := i
 		for j := i + 1; j < len(arr); j++ {
-			if arr[j] < arr[i] {
-				arr[i], arr[j] = arr[j], arr[i]
+			if arr[j] < arr[min] {
+				min = j
 			}
 		}
+		arr[i], arr[min] = arr[min], arr[i]
 	}
 }
 
 func main() {
-	arr := []int{4, 4, 1, 7, 2, 3}
+	arr := []int{4, 4, 1, 7, 2, 3, 0, 8}
 	sort(arr)
 	fmt.Println(arr)
 }
