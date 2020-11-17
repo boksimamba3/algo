f6543450ba65bae4f34a7d3beb46b9a93a778178
boksimamba3 <boriskostres0@gmail.com>
2020-11-17 15:47:04 +0100

    Check if number is palindrome

--- math/count-digits/main.go
+++ math/count-digits/main.go
@@ -7,26 +7,26 @@ import (
 
 // Question: Find a number of digits in a number
 // Example:  In: 9245, Out: 4;  In: 43256, Out: 5; In: 7, Out: 1;
-// Note: In(x) > 0
+// Note: In(n) > 0
 
 // Solution: Naive O(d) d = digits
 /*
 	Divide number by 10 until is greater then 0.
 	In = 798, Out = 3
-	x = 798, digits = 0
-	I1: x => 798/10 = 79 digits = 1
-	I2: x => 79/10 = 7 digits = 2
-	I3: x => 7/10 = 0 digits = 3
+	n = 798, digits = 0
+	I1: n => 798/10 = 79 digits = 1
+	I2: n => 79/10 = 7 digits = 2
+	I3: n => 7/10 = 0 digits = 3
 */
-/* func countDigits(x int) int {
-	if x <= 0 {
+/* func countDigits(n int) int {
+	if n <= 0 {
 		return 0
 	}
 
 	digits := 0
-	for x > 0 {
+	for n > 0 {
 		digits++
-		x = x / 10
+		n = n / 10
 	}
 
 	return digits
@@ -39,14 +39,14 @@ import (
 	and our number is between 10^2 and 10^3 or 100 and 1000.
 	log = log10(798) = 2.90200289135, digits = floor(log)+1
 	In: 798, Out: 3,
-	x = 798, digits = log10(in) + 1
+	n = 798, digits = log10(in) + 1
 */
-func countDigits(x int) int {
-	if x <= 0 {
+func countDigits(n int) int {
+	if n <= 0 {
 		return 0
 	}
 
-	log := math.Log10(float64(x))
+	log := math.Log10(float64(n))
 	return int(math.Floor(log) + 1)
 }
 
