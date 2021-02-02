bb8eccedd203ad073e4ea8376c0a065aef529a3c
boksimamba3 <boriskostres0@gmail.com>
2021-02-02 23:37:04 +0100

    Sorted insert and middle of linked list

---
+++ linked-list/middle-of-linked-list/main.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+// Question: Find middle element of linked list.
+
+// Solution: O(N)
+/*
+	We have two pointer. One slow and one fast. Slow moves
+	one element(node) at time while fast moves two elements(nodes).
+	When fast reaches end slow will reach the middle.
+*/
+
+type Element struct {
+	value int
+	next  *Element
+}
+
+func NewElement(value int) *Element {
+	return &Element{value: value}
+}
+
+type List struct {
+	head *Element
+}
+
+func NewList() *List {
+	return &List{}
+}
+
+func (l *List) Insert(value int) {
+	e := NewElement(value)
+	e.next = l.head
+	l.head = e
+}
+
+func (l *List) GetMiddle() int {
+	if l.head == nil {
+		return -1
+	}
+	slow := l.head
+	fast := l.head
+	for fast != nil && fast.next != nil {
+		slow = slow.next
+		fast = fast.next.next
+	}
+
+	return slow.value
+}
+
+func Print(l *List) {
+	for p := l.head; p != nil; p = p.next {
+		fmt.Println(p.value)
+	}
+}
+
+func main() {
+	list := NewList()
+	list.Insert(1)
+	list.Insert(3)
+	list.Insert(9)
+	list.Insert(4)
+	list.Insert(2)
+	list.Insert(5)
+	Print(list)
+	fmt.Printf("Middle element [ %v ]", list.GetMiddle())
+}
---
+++ linked-list/sorted-insert/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+// Question: Sorted insert in the linked list.
+// The linked list should remain sorted after insertion.
+
+// Solution: O(N)
+/*
+	To insert elements in sorted order we need to find prev element
+	that has value less then value we are inserting. After that we just
+	hook up all the pointers like in ordinary linked list.
+*/
+
+type Element struct {
+	value int
+	next  *Element
+}
+
+func NewElement(value int) *Element {
+	return &Element{value: value}
+}
+
+type List struct {
+	head *Element
+}
+
+func NewList() *List {
+	return &List{}
+}
+
+func (l *List) SortedInsert(value int) {
+	e := NewElement(value)
+	if l.head == nil {
+		e.next = l.head
+		l.head = e
+	} else {
+		p := l.head
+		for p.next != nil && p.next.value < value {
+			p = p.next
+		}
+		e.next = p.next
+		p.next = e
+	}
+}
+
+func Print(l *List) {
+	for p := l.head; p != nil; p = p.next {
+		fmt.Println(p.value)
+	}
+}
+
+func main() {
+	list := NewList()
+	list.SortedInsert(1)
+	list.SortedInsert(3)
+	list.SortedInsert(9)
+	list.SortedInsert(4)
+	list.SortedInsert(2)
+	Print(list)
+}
