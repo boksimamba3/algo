e37693ac13cb623df9ce9c49a8a6e35a904a3d4d
boksimamba3 <boriskostres0@gmail.com>
2021-02-09 23:16:07 +0100

    Reverse linked list in recursive manner

---
+++ linked-list/recursive-reverse/main.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+type Element struct {
+	value int
+	next  *Element
+}
+
+func NewElement(value int) *Element {
+	return &Element{value: value}
+}
+
+type List struct {
+	head *Element
+}
+
+func NewList() *List {
+	return &List{}
+}
+
+func (l *List) Insert(value int) {
+	e := NewElement(value)
+	e.next = l.head
+	l.head = e
+}
+
+// Question: Reverse a linked list in a recursive manner.
+
+// Solution: O(n)
+func (l *List) Reverse() {
+	var reverse func(head *Element) *Element
+	reverse = func(head *Element) *Element {
+		if head == nil || head.next == nil {
+			return head
+		}
+		newHead := reverse(head.next)
+		tail := head.next
+		tail.next = head
+		head.next = nil
+
+		return newHead
+	}
+
+	l.head = reverse(l.head)
+}
+
+func Print(l *List) {
+	for p := l.head; p != nil; p = p.next {
+		fmt.Println(p.value)
+	}
+}
+
+func main() {
+	list := NewList()
+	list.Insert(5)
+	list.Insert(4)
+	list.Insert(3)
+	list.Insert(2)
+	list.Insert(1)
+	Print(list)
+	fmt.Println("Reversed")
+	list.Reverse()
+	Print(list)
+}
